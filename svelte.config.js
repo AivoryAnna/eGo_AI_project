92ae36927ead571931fe921311ee0d6a1e3b5ad3
Anna Aivory <dubovikova.anastasia@icloud.com>
2024-02-13 20:04:50 +0400

    Configure Firebase for data storage and Vercel for hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
@@ -12,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: vercel(),
 		alias: {
 			$components: 'src/lib/components',
 			$pages: 'src/lib/pages'
